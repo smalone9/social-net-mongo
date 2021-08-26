eae4bb7ff3727044461288c4b07a18a19a24b894
Samantha Malone <sammac99@gmail.com>
2021-08-26 14:07:30 -0700

    added initial files

---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  User: require("./User"),
+  Reaction: require("./Reaction"),
+  Thought: require("./Thought"),
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const mongoose = require("mongoose");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-net", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log("Now listening on port 3001");
+});
